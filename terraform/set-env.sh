a94d81bbe9b6b2415bd29047bebb3e54f8596542
DevOps Engineer <100177153+sahibgasimov@users.noreply.github.com>
2022-03-26 18:26:20 -0500

    added script

--- terraform/set-env.sh
+++ terraform/set-env.sh
@@ -6,3 +6,22 @@ wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux_amd6
 sudo unzip terraform_0.14.7_linux_amd64.zip
 sudo chmod +x terraform_0.14.7_linux_amd64.zip
 sudo mv terraform /bin
+rm -rf terraform_0.14.7_linux_amd64.zip
+
+
+# install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl 
+sudo mv kubectl /bin/
+rm -rf 
+
+# install helm 
+    curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+    chmod 700 get_helm.sh
+    ./get_helm.sh
+    rm -rf get_helm.sh
+    rm -rf 
+
+# install nginx controller
+cd nginx-ingress-controller/
+helm install nginx-controller ./
