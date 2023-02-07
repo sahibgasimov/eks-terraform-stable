8f1a77f1f5b7968bc7f40193004d53c51821a4ac
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-06 23:26:32 -0600

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -11,16 +11,32 @@ terraform {
     }
   }
 }
+###############################create these 2 resources first 
+ resource "aws_s3_bucket" "mybucket" {
+    bucket = "s3statebackend-3212"
+    versioning {
+        enabled = true
+    }
+    server_side_encryption_configuration {
+        rule {
+            apply_server_side_encryption_by_default {
+                sse_algorithm = "AES256"
+            }
+        }
+    }
+}
 
-// Remote Backend Example
-// terraform {
-//   backend "s3" {
-//     bucket = "sgasimov-bucket"
-//     key    = "path/to/my/key"
-//     region = "us-east-1"
-//     dynamodb_table = "prod"
-//   }
-// }
+##create dynamodb
+resource "aws_dynamodb_table" "statelock" {
+    name = "state-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
 
 // Local Backend Example
 // terraform {
@@ -28,3 +44,27 @@ terraform {
 //    path = "./$HOME/$USER/terraform.tfstate"
 //  }
 // }
+
+#once above resources are created, replace line 5 code with the below to save tfstate on s3 backend 
+
+# terraform {
+#     required_version = ">= 0.14.7"
+#     backend "s3" {
+#         bucket = "s3statebackend-3212"
+#         dynamodb_table = "state-lock"
+#         key ="global/mystatefile/terraform.tfstate"
+#         region = "us-east-1"
+#         encrypt = true
+#     }
+#   required_providers {
+#     kubectl = {
+#       source  = "gavinbunney/kubectl"
+#       version = ">= 1.7.0"
+#     }
+
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
