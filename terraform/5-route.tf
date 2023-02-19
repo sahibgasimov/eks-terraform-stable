08631ea5ef7b65562b2a5ba638ab7d7f5d7a788e
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-18 22:32:50 -0500

    added routes to 1c zones

--- terraform/5-route.tf
+++ terraform/5-route.tf
@@ -60,6 +60,11 @@ resource "aws_route_table_association" "private-us-east-1b" {
   route_table_id = aws_route_table.private.id
 }
 
+resource "aws_route_table_association" "private-us-east-1c" {
+  subnet_id      = aws_subnet.private-us-east-1c.id
+  route_table_id = aws_route_table.private.id
+}
+
 resource "aws_route_table_association" "public-us-east-1a" {
   subnet_id      = aws_subnet.public-us-east-1a.id
   route_table_id = aws_route_table.public.id
@@ -67,5 +72,10 @@ resource "aws_route_table_association" "public-us-east-1a" {
 
 resource "aws_route_table_association" "public-us-east-1b" {
   subnet_id      = aws_subnet.public-us-east-1b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public-us-east-1c" {
+  subnet_id      = aws_subnet.public-us-east-1c.id
   route_table_id = aws_route_table.public.id
 }
