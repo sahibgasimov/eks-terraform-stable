845086d318bc841cf19ac753ebdb497f56db889b
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-19 01:45:30 -0500

    route53 allow policy for private nodes

--- terraform/7-nodes.tf
+++ terraform/7-nodes.tf
@@ -27,6 +27,12 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadO
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
   role       = aws_iam_role.nodes.name
 }
+# giving nodes permission to access route53
+resource "aws_iam_role_policy_attachment" "nodes-AmazonRoute53ReadOnlyAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53ReadOnlyAccess"
+  role       = aws_iam_role.nodes.name
+}
+
 
 resource "aws_eks_node_group" "private-nodes" {
   cluster_name    = aws_eks_cluster.demo.name
