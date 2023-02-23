58bce855b436048efa4fb00708f5db09868a3931
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-23 02:18:25 -0500

    Update 6-eks.tf

--- terraform/6-eks.tf
+++ terraform/6-eks.tf
@@ -31,7 +31,7 @@ resource "aws_eks_cluster" "demo" {
     subnet_ids = [
       aws_subnet.private-us-east-1a.id,
       aws_subnet.private-us-east-1b.id,
-      aws_subnet.private-us-east-1c.id
+      aws_subnet.private-us-east-1c.id,
       aws_subnet.public-us-east-1a.id,
       aws_subnet.public-us-east-1b.id,
       aws_subnet.public-us-east-1c.id
