86b651c8451c785ba9be2d4a1970d076c42acbb6
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-23 02:29:25 -0500

    Update 6-eks.tf

--- terraform/6-eks.tf
+++ terraform/6-eks.tf
@@ -17,6 +17,32 @@ resource "aws_iam_role" "demo" {
 POLICY
 }
 
+# allow eks control plane logging
+resource "aws_iam_policy" "eks_control_plane_logs" {
+  name        = "eks-control-plane-logs-policy"
+  policy      = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "eks_control_plane_logs" {
+  policy_arn = aws_iam_policy.eks_control_plane_logs.arn
+  role       = aws_iam_role.demo.name
+}
+
 resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.demo.name
@@ -25,7 +51,6 @@ resource "aws_iam_role_policy_attachment" "demo-AmazonEKSClusterPolicy" {
 resource "aws_eks_cluster" "demo" {
   name     = "demo"
   role_arn = aws_iam_role.demo.arn
-
 
   vpc_config {
     subnet_ids = [
@@ -37,8 +62,31 @@ resource "aws_eks_cluster" "demo" {
       aws_subnet.public-us-east-1c.id
     ]
   }
+#allow control-plane logging
+    logging {
+    cluster_logging {
+      enabled = true
 
+      types = [
+        "api",
+        "audit",
+        "authenticator",
+        "controllerManager",
+        "scheduler",
+      ]
+
+      # send logs to CloudWatch
+      cloudwatch_log_group_arn = aws_cloudwatch_log_group.eks_control_plane_logs.arn
+    }
+  }
+}
+  
+  
   depends_on = [aws_iam_role_policy_attachment.demo-AmazonEKSClusterPolicy]
+}
+
+resource "aws_cloudwatch_log_group" "eks_control_plane_logs" {
+  name = "/aws/eks/example/control-plane-logs"
 }
 
 output "create_aws_kubeconfig" {
