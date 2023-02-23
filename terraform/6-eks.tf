35d7d1911853819e8e5ad83d229ef928980dcd02
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-02-23 02:16:08 -0500

    Update 6-eks.tf

--- terraform/6-eks.tf
+++ terraform/6-eks.tf
@@ -26,12 +26,15 @@ resource "aws_eks_cluster" "demo" {
   name     = "demo"
   role_arn = aws_iam_role.demo.arn
 
+
   vpc_config {
     subnet_ids = [
       aws_subnet.private-us-east-1a.id,
       aws_subnet.private-us-east-1b.id,
+      aws_subnet.private-us-east-1c.id
       aws_subnet.public-us-east-1a.id,
-      aws_subnet.public-us-east-1b.id
+      aws_subnet.public-us-east-1b.id,
+      aws_subnet.public-us-east-1c.id
     ]
   }
 
