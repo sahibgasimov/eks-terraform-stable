583545f39789ff75c1b061a0b92ef87ce692cdbc
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-03 17:36:27 -0600

    Update 14-acm-certificate.tf

--- terraform/14-acm-certificate.tf
+++ terraform/14-acm-certificate.tf
@@ -1,41 +1,29 @@
-#resource "aws_acm_certificate" "example" {
- # domain_name = var.domain
-  #validation_method = "DNS"
-
-  #tags = {
-  #  Name = "example-cert"
-  #}
-#}
-
-#output "certificate_arn" {
- # value = aws_acm_certificate.example.arn
-#}
-
-
-resource "aws_route53_zone" "hello_world_zone" {
-  name = var.domain
-}
-resource "aws_acm_certificate" "hello_certificate" {
-  domain_name       = "*.${var.domain}"
+resource "aws_acm_certificate" "demo" {
+  domain_name       = var.domain
   validation_method = "DNS"
+  
+  tags = {
+    Environment = "prod"
+  }
+
   lifecycle {
     create_before_destroy = true
   }
-  tags = {
-    Name = var.domain
-  }
 }
 
-resource "aws_route53_record" "hello_cert_dns" {
-  allow_overwrite = true
-  name =  tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_name
-  records = [tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_value]
-  type = tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_type
-  zone_id = aws_route53_zone.hello_world_zone.zone_id
-  ttl = 60
+resource "aws_route53_record" "example_validation" {
+  name    = aws_acm_certificate.demo.domain_validation_options.0.resource_record_name
+  type    = aws_acm_certificate.demo.domain_validation_options.0.resource_record_type
+  zone_id = data.aws_route53_zone.demo.zone_id
+  records = [aws_acm_certificate.demo.domain_validation_options.0.resource_record_value]
+  ttl     = 60
 }
 
-resource "aws_acm_certificate_validation" "hello_cert_validate" {
-  certificate_arn = aws_acm_certificate.hello_certificate.arn
-  validation_record_fqdns = [aws_route53_record.hello_cert_dns.fqdn]
+data "aws_route53_zone" "demo" {
+  name = var.domain
+}
+
+resource "aws_acm_certificate_validation" "demo" {
+  certificate_arn         = aws_acm_certificate.demo.arn
+  validation_record_fqdns = [aws_route53_record.demo_validation.fqdn]
 }
