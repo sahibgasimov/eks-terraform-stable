3ccb0767e74593e0c78e6a8f9eb78669fb34ee35
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-03 17:54:15 -0600

    Update 14-acm-certificate.tf

--- terraform/14-acm-certificate.tf
+++ terraform/14-acm-certificate.tf
@@ -11,17 +11,6 @@ resource "aws_acm_certificate" "demo" {
   }
 }
 
-resource "aws_route53_record" "example_validation" {
-  name    = aws_acm_certificate.demo.domain_validation_options.0.resource_record_name
-  type    = aws_acm_certificate.demo.domain_validation_options.0.resource_record_type
-  zone_id = data.aws_route53_zone.demo.zone_id
-  records = [aws_acm_certificate.demo.domain_validation_options.0.resource_record_value]
-  ttl     = 60
-}
-
-data "aws_route53_zone" "demo" {
-  name = var.domain
-}
 
 resource "aws_acm_certificate_validation" "demo" {
   certificate_arn         = aws_acm_certificate.demo.arn
