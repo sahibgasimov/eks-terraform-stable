70e0bec38a2525a7a3c5b96277e10e116e77e119
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-03 19:44:18 -0600

    Update 14-acm-certificate.tf

--- terraform/14-acm-certificate.tf
+++ terraform/14-acm-certificate.tf
@@ -1,14 +1,27 @@
-module "cert" {
-  source = "github.com/azavea/terraform-aws-acm-certificate"
-
-
-  domain_name                       = var.domain
-  subject_alternative_names         = ["*.var.domain"]
-  hosted_zone_id                    = "${aws_route53_zone.default.zone_id}"
-  validation_record_ttl             = "60"
-  allow_validation_record_overwrite = true
+resource "aws_route53_zone" "hello_world_zone" {
+  name = var.domain
+}
+resource "aws_acm_certificate" "hello_certificate" {
+  domain_name       = "*.${var.domain}"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+  tags = {
+    Name = var.domain
+  }
 }
 
-  resource "aws_route53_zone" "default" {
-  name = "azavea.com"
+resource "aws_route53_record" "hello_cert_dns" {
+  allow_overwrite = true
+  name =  tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_name
+  records = [tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_value]
+  type = tolist(aws_acm_certificate.hello_certificate.domain_validation_options)[0].resource_record_type
+  zone_id = aws_route53_zone.hello_world_zone.zone_id
+  ttl = 60
+}
+
+resource "aws_acm_certificate_validation" "hello_cert_validate" {
+  certificate_arn = aws_acm_certificate.hello_certificate.arn
+  validation_record_fqdns = [aws_route53_record.hello_cert_dns.fqdn]
 }
