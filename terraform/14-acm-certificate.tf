b0292f1a31e76d6b63267051888324ab10e9bce7
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-03 18:04:03 -0600

    Update 14-acm-certificate.tf

--- terraform/14-acm-certificate.tf
+++ terraform/14-acm-certificate.tf
@@ -1,32 +1,10 @@
-resource "aws_acm_certificate" "example" {
-  domain_name       = var.domain
-  validation_method = "DNS"
+module "cert" {
+  source = "github.com/azavea/terraform-aws-acm-certificate"
 
-  tags = {
-    Environment = "prod"
-  }
 
-  lifecycle {
-    create_before_destroy = true
-  }
-
-}
-
-resource "aws_acm_certificate_validation" "example" {
-  certificate_arn         = aws_acm_certificate.example.arn
-  validation_record_fqdns = [aws_route53_record.example.fqdn]
-}
-
-resource "aws_route53_record" "example" {
-  name    = aws_acm_certificate.example.domain_validation_options[0].resource_record_name
-  type    = aws_acm_certificate.example.domain_validation_options[0].resource_record_type
-  zone_id = aws_route53_zone.example.zone_id
-  records = [aws_acm_certificate.example.domain_validation_options[0].resource_record_value]
-  ttl     = 60
-
-  depends_on = [aws_acm_certificate.example]
-}
-
-resource "aws_route53_zone" "example" {
-  name = var.domain
+  domain_name                       = var.domain
+  subject_alternative_names         = ["*.var.domain"]
+  hosted_zone_id                    = "${aws_route53_zone.default.zone_id}"
+  validation_record_ttl             = "60"
+  allow_validation_record_overwrite = true
 }
