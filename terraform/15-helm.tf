6c1a5dde6acb16841702c156d4976e28f5247564
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-05 03:29:42 -0600

    Update 15-helm.tf

--- terraform/15-helm.tf
+++ terraform/15-helm.tf
@@ -9,7 +9,7 @@ provider "helm" {
     }
   }
 }
-
+# aws loadbalancer controller
 resource "helm_release" "aws-load-balancer-controller" {
   name = "aws-load-balancer-controller"
 
@@ -42,4 +42,138 @@ resource "helm_release" "aws-load-balancer-controller" {
     aws_eks_node_group.private-nodes,
     aws_iam_role_policy_attachment.aws_load_balancer_controller_attach
   ]
+}
+
+##EXTERNAL DNS
+locals {
+  k8s = {
+    type    = "eks"
+    cluster = "demo"
+}
+}
+
+
+data "aws_caller_identity" "demo" {}
+
+data "aws_eks_cluster" "demo" {
+  name = local.k8s.cluster
+}
+
+
+data "aws_eks_cluster_auth" "aws_iam_authenticator" {
+  name = data.aws_eks_cluster.demo.name
+}
+
+data "aws_route53_zone" "demo" {
+  name = var.domain
+}
+
+resource "helm_release" "external-dns" {
+  name       = "external-dns"
+  namespace  = "kube-system"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "external-dns"
+  version    = "6.5.6"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.external_dns.arn
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "zoneType"
+    value = "public"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
+  }
+
+  set {
+    name  = "domainFilters[0]"
+    value = "cm
+  }
+
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "txtOwnerId" # TXT record identifier
+    value = "external-dns"
+  }
+}
+
+resource "aws_iam_role" "external_dns" {
+  name = "external-dns-dev"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${data.aws_caller_identity.demo.account_id}:oidc-provider/${replace(data.aws_eks_cluster.demo.identity[0].oidc[0].issuer, "https://", "")}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${replace(data.aws_eks_cluster.demo.identity[0].oidc[0].issuer, "https://", "")}:sub": "system:serviceaccount:kube-system:external-dns"
+        }
+      }
+    }
+  ]
+}
+  EOF
+  tags = {
+    Terraform = "true"
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns" {
+  role       = aws_iam_role.external_dns.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
+
+resource "aws_iam_policy" "external_dns" {
+  name        = "external-dns-dev"
+  description = "Policy using OIDC to give the EKS external dns ServiceAccount permissions to update Route53"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ChangeResourceRecordSets"
+     ],
+     "Resource": [
+       "arn:aws:route53:::hostedzone/*"
+     ]
+   },
+   {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ListHostedZones",
+       "route53:ListResourceRecordSets"
+     ],
+     "Resource": [
+       "*"
+     ]
+   }
+  ]
+}
+EOF
 }
