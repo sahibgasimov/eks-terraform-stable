3685ecc09dabd9fe7397a49832355a59c9569bcd
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-04 22:07:13 -0600

    Update 15-helm.tf

--- terraform/15-helm.tf
+++ terraform/15-helm.tf
@@ -1,10 +1,10 @@
 provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.demo.endpoint
-    cluster_ca_certificate = base64decode(aws_eks_cluster.cluster.certificate_authority[0].data)
+    cluster_ca_certificate = base64decode(aws_eks_cluster.demo.certificate_authority[0].data)
     exec {
       api_version = "client.authentication.k8s.io/v1beta1"
-      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.cluster.id]
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.demo.id]
       command     = "aws"
     }
   }
