bdc7464134ef947af05bf4799f66b7f365757441
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-19 00:33:19 -0500

    Update ingress_and_externaldns.tf

--- terraform/helm/ingress_and_externaldns.tf
+++ terraform/helm/ingress_and_externaldns.tf
@@ -1,1 +1,179 @@
+provider "helm" {
+  kubernetes {
+    host                   = aws_eks_cluster.dev.endpoint
+    cluster_ca_certificate = base64decode(aws_eks_cluster.dev.certificate_authority[0].data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.dev.id]
+      command     = "aws"
+    }
+  }
 
+}
+#aws loadbalancer controller
+resource "helm_release" "aws-load-balancer-controller" {
+  name = "aws-load-balancer-controller"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "kube-system"
+  version    = "1.4.8"
+
+  set {
+    name  = "clusterName"
+    value = aws_eks_cluster.dev.id
+  }
+
+  set {
+    name  = "image.tag"
+    value = "v2.4.7"
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "aws-load-balancer-controller"
+  }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.aws_load_balancer_controller.arn
+  }
+
+  depends_on = [
+    aws_eks_node_group.private-nodes,
+    aws_iam_role_policy_attachment.aws_load_balancer_controller_attach
+  ]
+}
+
+#External DNS
+locals {
+  k8s = {
+    type    = "eks"
+    cluster = "dev"
+  }
+}
+
+data "aws_caller_identity" "dev" {}
+
+data "aws_eks_cluster" "dev" {
+  name = local.k8s.cluster
+}
+
+data "aws_eks_cluster_auth" "aws_iam_authenticator" {
+  name = data.aws_eks_cluster.dev.name
+}
+
+/* data "aws_route53_zone" "dev" {
+  vpc_id = aws_vpc.main.id
+  name = var.domain
+} */
+
+resource "helm_release" "external-dns" {
+  name       = "external-dns"
+  namespace  = "kube-system"
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "external-dns"
+  version    = "6.14.3"
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = aws_iam_role.external_dns.arn
+  }
+
+  set {
+    name  = "serviceAccount.name"
+    value = "external-dns"
+  }
+
+  set {
+    name  = "zoneType"
+    value = "public"
+  }
+
+  set {
+    name  = "policy"
+    value = "sync"
+  }
+
+  set {
+    name  = "domainFilters[0]"
+    value = var.domain
+  }
+
+  set {
+    name  = "provider"
+    value = "aws"
+  }
+
+  set {
+    name  = "txtOwnerId" #TXT record identifier
+    value = "external-dns"
+  }
+}
+
+resource "aws_iam_role" "external_dns" {
+  name = "external-dns-dev"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::${data.aws_caller_identity.dev.account_id}:oidc-provider/${replace(data.aws_eks_cluster.dev.identity[0].oidc[0].issuer, "https://", "")}"
+      },
+      "Action": "sts:AssumeRoleWithWebIdentity",
+      "Condition": {
+        "StringEquals": {
+          "${replace(data.aws_eks_cluster.dev.identity[0].oidc[0].issuer, "https://", "")}:sub": "system:serviceaccount:kube-system:external-dns"
+        }
+      }
+    }
+  ]
+}
+  EOF
+  tags = {
+    Terraform = "true"
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "external_dns" {
+  role       = aws_iam_role.external_dns.name
+  policy_arn = aws_iam_policy.external_dns.arn
+}
+
+resource "aws_iam_policy" "external_dns" {
+  name        = "external-dns-dev"
+  description = "Policy using OIDC to give the EKS external dns ServiceAccount permissions to update Route53"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+  {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ChangeResourceRecordSets"
+     ],
+     "Resource": [
+       "arn:aws:route53:::hostedzone/*"
+     ]
+   },
+   {
+     "Effect": "Allow",
+     "Action": [
+       "route53:ListHostedZones",
+       "route53:ListResourceRecordSets"
+     ],
+     "Resource": [
+       "*"
+     ]
+   }
+  ]
+}
+EOF
+}
