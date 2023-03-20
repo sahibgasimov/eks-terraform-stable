8cda8f8f97413f6d58142850877d571c7cbb1702
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-03-20 00:49:40 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,6 +16,3 @@ variable "private_subnet_3" {}
 variable "public_subnet_1" {}
 variable "public_subnet_2" {}
 variable "public_subnet_3" {}
-variable "alb_ingress_controller_version" {}
-variable "external_dns_version" {}
-variable "alb_ingress_tag" {}
