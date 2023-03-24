5d0565d2c41126321c322db1a3aee63a321a6206
sahib.gasimov2 <sahib.gasimov2@gmail.com>
2023-03-24 02:24:36 -0500

    added csi

--- terraform/8-csi-driver-iam.tf
+++ terraform/8-csi-driver-iam.tf
@@ -31,6 +31,10 @@ resource "aws_eks_addon" "csi_driver" {
   addon_name               = "aws-ebs-csi-driver"
   addon_version            = "v1.15.1-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
+
+  depends_on = [
+    aws_eks_cluster.dev
+  ]
 }
 
 # Optional: only if you use your own KMS key to encrypt EBS volumes
