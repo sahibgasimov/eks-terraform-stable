46a449d95df11b2fab183cd5631e3e43f164f0f7
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-08-16 03:34:37 -0400

    Create argocd.tf

---
+++ addons/argocd/argocd.tf
@@ -0,0 +1,23 @@
+ provider "helm" {
+   kubernetes {
+     host                   = aws_eks_cluster.demo.endpoint
+     cluster_ca_certificate = base64decode(aws_eks_cluster.demo.certificate_authority[0].data)
+     exec {
+       api_version = "client.authentication.k8s.io/v1beta1"
+       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.demo.id]
+       command     = "aws"
+     }
+   }
+ }
+
+# provider "helm" {
+   kubernetes {
+     host                   = aws_eks_cluster.dev.endpoint
+     cluster_ca_certificate = base64decode(aws_eks_cluster.dev.certificate_authority[0].data)
+     exec {
+       api_version = "client.authentication.k8s.io/v1beta1"
+       args        = ["eks", "get-token", "--cluster-name", aws_eks_cluster.dev.id]
+       command     = "aws"
+     }
+   }
+ }
