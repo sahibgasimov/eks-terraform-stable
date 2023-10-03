aa12b4ba9a173e2c52f2b34ef252b93af5588c70
SG <100177153+sahibgasimov@users.noreply.github.com>
2023-10-03 01:16:50 -0500

    Update moduel.tf

--- addons/loki/tf_loki/module/moduel.tf
+++ addons/loki/tf_loki/module/moduel.tf
@@ -1,7 +1,7 @@
 module "loki" {
   source        = "../" # Path to the module folder
   bucket_name   = "sdsdsd-sah-loki-object-store"
-  oidc_provider = "arn:aws:iam::292061018164:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/F488F53EF1EB2C667F162F94D29E7337"
+  oidc_provider = "arn:aws:iam:::oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/F488F53EF1EB2C667F162F94D29E7337"
   chart_version = "5.22"
   promtail_chart_version = "6.15"
   cluster       = "dev"
