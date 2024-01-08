7ecd6542fd4d79deb8813308200bcd8aa53f75f8
SG <100177153+sahibgasimov@users.noreply.github.com>
2024-01-08 02:13:39 -0600

    Update example.tf

--- terraform/module/example.tf
+++ terraform/module/example.tf
@@ -2,13 +2,13 @@ module "eks" {
   source = "github.com/sahibgasimov/eks-terraform-stable//terraform?ref=main" 
   #### EKS Cluster ####
   cluster_name    = "dev"
-  cluster_version = "1.26"
+  cluster_version = "1.28"
   environment     = "dev"
   ##### ALB Ingress Controller and External DNS #####
-  external_dns          = "6.20.3"
-  alb_ingress           = "1.5.3"
-  alb_ingress_image_tag = "v2.5.2"
-  csi_driver = "v1.19.0-eksbuild.1"
+  external_dns          = "6.28.5"
+  alb_ingress           = "1.6.1"
+  alb_ingress_image_tag = "v2.6.1"
+  csi_driver = "v1.26.0-eksbuild.1"
   ##### Nodes Autoscaling desired instance size #####
   instance_types  = "t3.small"
   desired_size    = 2
@@ -17,8 +17,8 @@ module "eks" {
   max_unavailable = 1
   ##### Route53 Domain #####
   region         = "us-east-1"
-  domain         = "cmcloudlab1626.info"
-  hosted_zone_id = "Z02775881FI0DIF13QOF5"
+  domain         = "349085342913.realhandsonlabs.net"
+  hosted_zone_id = "Z10013141BU1GNFS3VGI9"
   ##### Networking #####
   vpc_cidr         = "10.0.0.0/16"
   private_subnet_1 = "10.0.0.0/19"
