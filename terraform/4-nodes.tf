144194fdd36d5f42582366c49bea4a6d811eb29b
SG <100177153+sahibgasimov@users.noreply.github.com>
2024-05-27 22:12:36 -0400

    Update 4-nodes.tf

--- terraform/4-nodes.tf
+++ terraform/4-nodes.tf
@@ -71,7 +71,9 @@ resource "aws_eks_node_group" "private-nodes" {
   cluster_name    = aws_eks_cluster.dev.name
   node_group_name = "${var.cluster_name}-private-nodes"
   node_role_arn   = aws_iam_role.nodes.arn
-
+  timeouts {
+    create = "60m"
+  }
   subnet_ids = [
     aws_subnet.private-1.id,
     aws_subnet.private-2.id,
